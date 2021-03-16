87ec457c521e95fbe8e5cf137333da5f64c4f1b5
awjae <djwotmd@gmail.com>
2021-03-16 23:42:29 +0900

    add markdown render and dynamic render

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,17 +1,20 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
 
-export default function Post({ postData }) {
-  return (
-    <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-    </Layout>
-  )
-}
+// export default function Post({ postData }) {
+//   return (
+//     <Layout>
+//       {postData.title}
+//       <br />
+//       {postData.id}
+//       <br />
+//       {postData.date}
+//       <br />
+//       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+//     </Layout>
+//   )
+// }
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
@@ -22,10 +25,29 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
     }
   }
+}
+import Head from 'next/head'
+import Date from '../../components/date'
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
 }
