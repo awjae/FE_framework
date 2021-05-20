5d58c6a66422df9aea75468aa944bc986b28e7aa
awjae <djwotmd@gmail.com>
2021-05-20 20:53:29 +0900

    use tsx generic

--- gatsby/src/pages/index.tsx
+++ gatsby/src/pages/index.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from 'react';
 import Text from 'components/Text';
 
 const IndexPage: FunctionComponent = function () {
-  return <Text text="Home" />;
+  return <Text text={'10'} />; // error TS2322: Type 'number' is not assignable to type 'string'.
 };
 
 export default IndexPage;
