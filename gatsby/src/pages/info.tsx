d4cad86f16206293d470855c9e8c53eb323ea5e7
awjae <djwotmd@gmail.com>
2021-05-25 16:33:01 +0900

    add emotionjs css with props

--- gatsby/src/pages/info.tsx
+++ gatsby/src/pages/info.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent } from 'react';
 import { graphql } from 'gatsby';
 import { Link } from 'gatsby';
+import styled from '@emotion/styled';
+import { Global, css } from '@emotion/react';
 
 interface InfoPageProps {
   data: {
@@ -29,7 +31,10 @@ const InfoPage: FunctionComponent<InfoPageProps> = function ({
     //   <Link to="/">To Main</Link>
     // </div>
     <div>
-      {title} {description} {author}
+      <Global styles={globalStyle} />
+      <div css={TextStyle}>{title}</div>
+      <Text1 disable={true}>{description}</Text1>
+      <Text2 disable={true}>{author}</Text2>
     </div>
   );
 };
@@ -47,3 +52,38 @@ export const metadataQuery = graphql`
     }
   }
 `;
+
+const globalStyle = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    font-size: 20px;
+  }
+`;
+const TextStyle = css`
+  font-size: 18px;
+  font-weight: 700;
+  color: gray;
+`;
+// const Text1 = styled.div`
+//   font-size: 20px;
+//   font-weight: 700;
+// `;
+// // Camel Case 적용
+// const Text2 = styled('div')(() => ({
+//   fontSize: '15px',
+//   color: 'blue',
+// }));
+const Text1 = styled.div<{ disable: boolean }>`
+  font-size: 20px;
+  font-weight: 700;
+  text-decoration: ${({ disable }) => (disable ? 'line-through' : 'none')};
+`;
+
+const Text2 = styled('div')<{ disable: boolean }>(({ disable }) => ({
+  fontSize: '15px',
+  color: 'blue',
+  textDecoration: disable ? 'line-through' : 'none',
+}));
