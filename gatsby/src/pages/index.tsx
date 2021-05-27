320e0e4dce2098cc3dac0772d2ae80d447bdc623
awjae <djwotmd@gmail.com>
2021-05-27 23:26:26 +0900

    styled components width props

--- gatsby/src/pages/index.tsx
+++ gatsby/src/pages/index.tsx
@@ -1,26 +1,30 @@
 import React, { FunctionComponent } from 'react';
 import styled from '@emotion/styled';
 import GlobalStyle from 'components/Common/GlobalStyle';
-import Introduction from 'components/Main/Introduction';
-import ProfileImage from 'components/Main/ProfileImage';
 import Footer from 'components/Common/Footer';
+import CategoryList from 'components/Main/CategoryList';
+import Introduction from 'components/Main/Introduction';
+
+const CATEGORY_LIST = {
+  All: 5,
+  Web: 3,
+  Mobile: 2,
+};
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
+  height: 100vh;
 `;
 
 const IndexPage: FunctionComponent = function () {
   return (
     <Container>
       <GlobalStyle />
-      <Introduction  />
+      <Introduction />
+      <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
       <Footer />
     </Container>
-  //   <div>
-  //   <Link to="/info/">To Info</Link>
-  // </div>
   );
 };
 
