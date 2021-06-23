d2fb2855ec68c48e827ec8932f16ac9dda94ccd5
awjae <djwotmd@gmail.com>
2021-06-23 22:55:48 +0900

    vue route, code splitting and lazyloading and prefetching

--- vuejs/vue-default/README.md
+++ vuejs/vue-default/README.md
@@ -22,3 +22,9 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+
+
+1. 라우터
+2. lazyloading
--- vuejs/vue-default/src/router/index.js
+++ vuejs/vue-default/src/router/index.js
@@ -14,12 +14,15 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
+    // 라우터 레벨에서 코드를 스플리팅하고, 방문시 라우팅된다(lazyloading), 프리패치(app.js, about.js 별도 파일) 캐시에만 존재하다가 사용자(클라이언트)가 사용하게되면 메모리에 위치
+    // ★★★ 프리패치를 할지, 그능을 끌지 적절하게 선택해야한다
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/Contact',
     name: 'Contact',
-    component: Contact
+    // component: Contact
+    component: () => import(/* webpackChunkName: "contact", webpackPrefetch: true */ '../views/About.vue')
   },
 ]
 
