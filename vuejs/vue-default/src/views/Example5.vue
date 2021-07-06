2c27c8ec57e0b2f382dc49a97ad679a0bbc35fb9
awjae <djwotmd@gmail.com>
2021-07-06 22:17:09 +0900

    seach example

---
+++ vuejs/vue-default/src/views/Example5.vue
@@ -0,0 +1,59 @@
+<!-- vue 컴포넌트의 기본 형태 -->
+<template>
+    <div>
+        <input type="text" v-model="name"/>
+        <input type="text" v-model.number="age" />
+        <input type="text" v-model="job" />
+        <button type="button" @click="saveUSerInfo">저장</button>
+    </div>
+</template>
+<script>
+export default {
+    name:'',
+    components: {}, 
+    data() {
+        return {
+          name:'',
+          age:0,
+          job:'',
+          userInfo: {
+              name: '',
+              age:0,
+              job:''
+          }
+        };
+    },
+    setup() {},
+    created() {}, 
+    mounted() {}, 
+    unmounted() {},
+    methods: {
+        saveUserInfo() {
+            if(this.name === '') {
+                return alert('사용자 이름을 입력하세요');
+            }
+            if(this.age === 0 || this.age === '') {
+                return alert('사용자 나이을 입력하세요');
+            }
+
+            const params = {
+                name: this.name,
+                age: this.age,
+                job: this.job
+            }
+            const r = this.saveData(params);
+            if(r === "S") {
+                alert('정상적으로 사용자 정보가 생성되었습니다.')
+            }
+        },
+        saveData(params) {
+            console.log(params)
+            const r = "S";
+            return r;
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ vuejs/vue-default/src/views/Search.vue
@@ -0,0 +1,78 @@
+<!-- vue 컴포넌트의 기본 형태 -->
+<template>
+    <div>
+        <div>
+            <input type="text" v-model="title" />
+            <select v-model="selectedCity">
+                <option :value="city.code" :key="i" v-for="(city, i) in cityList">{{ city.name }}</option>
+            </select>
+            <input type="text" v-model="userName" @keyup.enter="searchUserList" />
+            <button type="button" @click="searchUserList">조회</button>
+        </div>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>이름</th>
+                        <th>나이</th>
+                        <th>직업</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr :key="i" v-for="(person, i) in userList">
+                        <td>{{ person.name }}</td>
+                        <td>{{ person.age }}</td>
+                        <td>{{ person.job }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'',
+    components: {}, //하나의 컴포넌트는 다른 컴포넌트에서 쉽게 불러와 사용할 수 있다.
+    data() {
+        return {
+          cityLisy: [
+            { name:"서울", code:"01" },
+            { name:"대구", code:"02" },
+            { name:"제주", code:"03" }
+          ],
+          selectedCity: "",
+          userName: "",
+          userList: []
+        };
+    },
+    setup() {},
+    created() {}, //컴포넌트가 처음 실행된순간
+    mounted() {}, // 컴포넌트의  dom들이 완성된순간
+    unmounted() {},
+    methods: {
+        searchUserList() {
+            // const selectedCity = this.selectedCity;
+            // const userName = this.userName;
+            const params = {
+                selectedCity: this.selectedCity,
+                useName: this.useName
+            }
+
+            this.userList = this.getUserList(params);
+        },
+        getUserList(params) {
+            console.log(params)
+            let list = [
+                {name:'유재석', age: 50, job: '얀에인'},
+                {name:'하하', age: 35, job: '가수'},
+                {name:'정준하', age: 34, job: '사수'},
+            ];
+
+            return list;
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
