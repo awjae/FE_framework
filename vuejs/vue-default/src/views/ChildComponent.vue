c3c25e36dc4e96a2258478dbb97765db096d753f
awjae <djwotmd@gmail.com>
2021-07-10 20:51:09 +0900

    동적 바인딩 with props

---
+++ vuejs/vue-default/src/views/ChildComponent.vue
@@ -0,0 +1,46 @@
+<!-- vue 컴포넌트의 기본 형태 -->
+<template>
+    <div>
+        <h1>likes : {{ likes }}</h1>
+        <h1>isOk : {{ isOk }}</h1>
+        <h1>commentIdx : {{ commentIdx }}</h1>
+        <h1>author name : {{ author.name }}</h1>
+    </div>
+</template>
+<script>
+export default {
+    name:'',
+    props: {
+        likes: {
+            type: Number,
+            default: 0
+        },
+        isOk: {
+            type: Boolean,
+            default: false
+        },
+        commentIdx: {
+            type: Array
+        },
+        author: {
+            type: Object
+        }
+    },
+    components: {}, //하나의 컴포넌트는 다른 컴포넌트에서 쉽게 불러와 사용할 수 있다.
+    data() {
+        return {
+          title: 'Hello world' //양방향 바인딩 
+        };
+    },
+    setup() {},
+    created() {}, //컴포넌트가 처음 실행된순간
+    mounted() {
+        console.log(this.commentIdx);
+    }, // 컴포넌트의  dom들이 완성된순간
+    unmounted() {},
+    methods: {}
+}
+</script>
+<style scoped>
+
+</style>
--- vuejs/vue-default/src/views/Example6.vue
+++ vuejs/vue-default/src/views/Example6.vue
@@ -1,18 +1,25 @@
 <!-- vue 컴포넌트의 기본 형태 -->
 <template>
     <div>
-        <PageTitle title="부모 컴포넌트에서 전송할 페이지 타이틀" />
+        <page-title title="부모 컴포넌트에서 전송할 페이지 타이틀" />
+        <page-title :title="title" />
+        <ChildComponent :likes="likes" :isOk="isOk" :commentIdx="commentIdx" :author="author"/> <!-- 동적 바인딩을 하면 숫자필드, 불리언 필드로 데이터 전송 가능 -->
     </div>
 </template>
 <script>
 import PageTitle from '../components/PageTitle.vue';
+import ChildComponent from './ChildComponent.vue';
 
 export default {
     name:'',
-    components: { PageTitle }, //하나의 컴포넌트는 다른 컴포넌트에서 쉽게 불러와 사용할 수 있다.
+    components: { 'page-title' : PageTitle, ChildComponent }, //하나의 컴포넌트는 다른 컴포넌트에서 쉽게 불러와 사용할 수 있다.
     data() {
         return {
-          title: 'Hello world' //양방향 바인딩 
+          title: 'Hello world',
+          likes: 23,
+          isOk: true,
+          commentIdx: [1,5,4,2],
+          author: { name: '홍길동', company: '회사이름' }
         };
     },
     setup() {},
