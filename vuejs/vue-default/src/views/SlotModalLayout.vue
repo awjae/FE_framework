1327d2d01a0f7af1e7f55256cffe2abad72a537d
awjae <djwotmd@gmail.com>
2021-07-19 19:50:33 +0900

    use slot

---
+++ vuejs/vue-default/src/views/SlotModalLayout.vue
@@ -0,0 +1,14 @@
+<!-- slot : 부모에서 slot 부분을 재정의가 가능 -->
+<template>
+    <div class="modal-container">
+        <header>
+            <slot name="header"></slot>
+        </header>
+        <main>
+            <slot></slot>
+        </main> 
+        <footer>
+            <slot name="footer"></slot>
+        </footer>
+    </div>
+</template>
---
+++ vuejs/vue-default/src/views/SlotUseModalLayout.vue
@@ -0,0 +1,36 @@
+<!-- slot 재사용 -->
+<template>
+    <modal-layout>
+        <template v-slot:header>
+            <h1>팝업타이틍</h1>
+        </template>
+        <template v-slot:default>
+            <p>팝업컨텐츠1</p>
+            <p>팝업컨텐츠2</p>
+        </template>
+        <template v-slot:footer>
+            <button type="button">닫기</button>
+        </template>
+    </modal-layout>
+</template>
+<script>
+import SlotModalLayout from "./SlotModelLayout.vue";
+
+export default {
+    name:'',
+    components: { "modal-layout": SlotModalLayout}, //하나의 컴포넌트는 다른 컴포넌트에서 쉽게 불러와 사용할 수 있다.
+    data() {
+        return {
+          title: 'Hello world' //양방향 바인딩 
+        };
+    },
+    setup() {},
+    created() {}, //컴포넌트가 처음 실행된순간
+    mounted() {}, // 컴포넌트의  dom들이 완성된순간
+    unmounted() {},
+    methods: {}
+}
+</script>
+<style scoped>
+
+</style>
