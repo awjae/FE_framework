9d0f178bf8e5a323313cb5d7cd27687710c24197
awjae <djwotmd@gmail.com>
2022-05-01 16:19:26 +0900

    Destroy

--- svelte/src/App_2.svelte
+++ svelte/src/App_2.svelte
@@ -1,12 +1,17 @@
 <script>
     import Bindings from "./components/Bindings.svelte";
-    import { onMount } from 'svelte';
+    import { onMount, onDestroy } from 'svelte';
 
 	let photos = [];
     onMount(async () => {
 		const res = await fetch(`/tutorial/api/album`);
 		photos = await res.json();
 	});
+
+    let counter = 0;
+	const interval = setInterval(() => counter += 1, 1000);
+
+	onDestroy(() => clearInterval(interval));
 </script>
 <main>
     <h2>Page 2</h2>
@@ -21,6 +26,7 @@
             <!-- this block renders when photos.length === 0 -->
             <p>loading...</p>
         {/each}
+        <p>{counter}</p>
     </div>
     
 </main>
