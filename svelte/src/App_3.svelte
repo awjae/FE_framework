0f13f00d412e0a4d34b32ef1d159dd322f66f5f8
AW <djwotmd@gmail.com>
2022-05-13 17:05:14 +0900

    readStore add

--- svelte/src/App_3.svelte
+++ svelte/src/App_3.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { count } from './store/stores.js';
+	import { time } from './store/readStore.js';
 	import Incrementer from './components/Incrementer.svelte';
 	import Decrementer from './components/Decrementer.svelte';
 	import Resetter from './components/Resetter.svelte';
@@ -9,10 +10,20 @@
 	count.subscribe(value => {
 		countValue = value;
 	});
+
+
+	const formatter = new Intl.DateTimeFormat('en', {
+		hour12: true,
+		hour: 'numeric',
+		minute: '2-digit',
+		second: '2-digit'
+	});
 </script>
 
 <h1>The count is {countValue}</h1>
-
 <Incrementer/>
 <Decrementer/>
 <Resetter/>
+
+
+<h1>The time is {formatter.format($time)}</h1>
