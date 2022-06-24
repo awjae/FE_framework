4d339e0233af0791cfb2d9eafc34ca7f712c3a52
awjae <djwotmd@gmail.com>
2022-06-24 23:11:15 +0900

    start RN App

---
+++ reactNativeInit/src/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  console.log(12321312)
+  return (
+    <View style={styles.container}>
+      <Text>Change dir, </Text>
+      <Text style={{ color: 'blue' }}>Hi</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
