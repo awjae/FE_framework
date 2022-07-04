eab5a0dec239af0b1203cb010eb6f39134349e94
awjae <djwotmd@gmail.com>
2022-07-04 21:21:18 +0900

    add Task components

--- reactNativeInit/src/App.js
+++ reactNativeInit/src/App.js
@@ -6,6 +6,7 @@ import Input from './components/Input';
 import { useState } from 'react';
 import IconButton from './components/IconButton';
 import { icons } from './icons';
+import Task from './components/Task';
 
 const Container= styled.View`
   flex: 1;
@@ -23,12 +24,20 @@ const Title = styled.Text`
   padding: 0 20px;
 `;
 
+const List = styled.ScrollView`
+  flex: 1;
+  width: ${({ width }) => width - 40}px;
+`;
+
+
 export default function App() {
   const [newTask, setNewTask] = useState('');
   const addTask = () => {
     alert(newTask);
     setNewTask('');
   }
+  const width = Dimensions.get('window').width;
+
 
   return (
     // <View style={styles.container}>
@@ -53,6 +62,16 @@ export default function App() {
         <IconButton icon={icons.unCheck} onPress={() => alert('unCheck')}></IconButton>
         <IconButton icon={icons.edit} onPress={() => alert('edite')}></IconButton>
         <IconButton icon={icons.delete} onPress={() => alert('delete')}></IconButton>
+        <List width={width}>>
+          <Task text="test1"></Task>
+          <Task text="test2"></Task>
+          <Task text="test3"></Task>
+          <Task text="test4"></Task>
+          <Task text="test5"></Task>
+          <Task text="test6"></Task>
+          <Task text="test7"></Task>
+          <Task text="test8"></Task>
+        </List>
       </Container>  
     </ThemeProvider>
   );
