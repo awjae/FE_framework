e9f3b22a7eb38b85252e95e26ba328176ef84d8d
awjae <djwotmd@gmail.com>
2022-10-31 20:35:54 +0900

    components 화

---
+++ reactNativeInit/src/components/ListHorizontal.js
@@ -0,0 +1,58 @@
+
+import { Image, ScrollView, StyleSheet } from 'react-native';
+
+function ListHorizontal() {
+  return (
+    <ScrollView 
+        style={styles.container}
+        horizontal={true}
+        >
+        {/* <Image
+          style={styles.tinyLogo}
+          source={require('@expo/snack-static/react-native-logo.png')}
+        /> */}
+        <Image
+          style={styles.image}
+          source={require('../../assets/favicon.png')}
+        />
+
+        <Image
+          style={styles.image}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
+
+        <Image
+          style={styles.image}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
+
+        <Image
+          style={styles.image}
+          source={{
+            uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
+          }}
+        />
+      </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    display: 'flex',
+    flexDirection: 'row'
+    // overflow: 'auto',
+  },
+  image: {
+    width: 150,
+    margin: 10,
+    height: 150,
+    //display: 'inline-block', Failed %s type: %s%s, prop, Invalid prop `display` of value `inline-block` supplied to `Image`, expected one of ["none","flex"]
+  },
+});
+
+export default ListHorizontal
--- reactNativeInit/src/pages/main.js
+++ reactNativeInit/src/pages/main.js
@@ -1,45 +1,13 @@
 
 import React from 'react';
-import { View, Image, StyleSheet, ScrollView, SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import ListHorizontal from '../components/ListHorizontal';
 
 const DisplayAnImage = () => {
+
   return (
     <SafeAreaView style={styles.safeArea}>
-      <ScrollView 
-        style={styles.container}
-        horizontal={true}
-        >
-        {/* <Image
-          style={styles.tinyLogo}
-          source={require('@expo/snack-static/react-native-logo.png')}
-        /> */}
-        <Image
-          style={styles.image}
-          source={require('../../assets/favicon.png')}
-        />
-
-        <Image
-          style={styles.image}
-          source={{
-            uri: 'https://reactnative.dev/img/tiny_logo.png',
-          }}
-        />
-
-        <Image
-          style={styles.image}
-          source={{
-            uri: 'https://reactnative.dev/img/tiny_logo.png',
-          }}
-        />
-
-        <Image
-          style={styles.image}
-          source={{
-            uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
-          }}
-        />
-
-      </ScrollView>
+      <ListHorizontal></ListHorizontal>
     </SafeAreaView>
   );
 }
@@ -47,19 +15,7 @@ const DisplayAnImage = () => {
 const styles = StyleSheet.create({
   safeArea: {
     padding: 10, 
-  },
-  container: {
-    padding: 10,
-    display: 'flex',
-    flexDirection: 'row'
-    // overflow: 'auto',
-  },
-  image: {
-    width: 150,
-    margin: 10,
-    height: 150,
-    //display: 'inline-block', Failed %s type: %s%s, prop, Invalid prop `display` of value `inline-block` supplied to `Image`, expected one of ["none","flex"]
-  },
-});
+  }
+})
 
 export default DisplayAnImage;
