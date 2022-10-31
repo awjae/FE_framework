5a15b0fa27e7b4159099fd385bb514f0a5fb934a
awjae <djwotmd@gmail.com>
2022-10-31 20:43:50 +0900

    perission layer

--- reactNativeInit/src/pages/main.js
+++ reactNativeInit/src/pages/main.js
@@ -1,13 +1,46 @@
 
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, View, PermissionsAndroid } from 'react-native';
 import ListHorizontal from '../components/ListHorizontal';
 
+const requestCameraPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+      {
+        title: "카메라 권한 주세요",
+        message:
+          "카메라 권한 주면 착한 사람",
+        buttonNeutral: "나중에",
+        buttonNegative: "취소",
+        buttonPositive: "허용"
+      }
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log("수락");
+    } else {
+      console.log("거절");
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+};
+
 const DisplayAnImage = () => {
+
+  const goCameraHandler = () => {
+    requestCameraPermission();
+  }
 
   return (
     <SafeAreaView style={styles.safeArea}>
       <ListHorizontal></ListHorizontal>
+      <View>
+        <Button 
+          title="카메라"
+          onPress={goCameraHandler}
+        />
+      </View>
     </SafeAreaView>
   );
 }
